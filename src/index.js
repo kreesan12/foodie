8d79c121fa5e0977738bb88cecffbe00c696f829
Kreesan Govender <kreesangovender12@gmail.com>
2024-08-07 00:44:30 +0200

    new9

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { BasketProvider } from './contexts/BasketContext';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
