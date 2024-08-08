4255623f0ba3bb5c10a880ed74e61cf0a8414c27
Kreesan Govender <kreesangovender12@gmail.com>
2024-08-09 01:55:35 +0200

    h2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
+import React, { useEffect, useContext } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
 import Menu from './components/Menu';
@@ -10,10 +10,12 @@ import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import SectionSelector from './components/SectionSelector';
-import { BasketProvider } from './contexts/BasketContext';
+import { BasketProvider, BasketContext } from './contexts/BasketContext';
 import './App.css';
 
 function App() {
+    const { basket } = useContext(BasketContext); // Access basket from context
+
     useEffect(() => {
         fetch('https://fathomless-retreat-07632-66acd80d626f.herokuapp.com')
             .then(response => response.json())
@@ -31,13 +33,13 @@ function App() {
                             <Carousel />
                             <SectionSelector />
                             <Menu />
+                            {basket.length > 0 && <FrontPageBasket />} {/* Show only if items in basket */}
                         </Route>
                         <Route path="/checkout" component={Checkout} />
                         <Route path="/orders" component={Orders} />
                         <Route path="/about-us" component={AboutUs} />
                         <Route path="/contact-us" component={ContactUs} />
                     </Switch>
-                    <FrontPageBasket />
                     <Footer />
                 </div>
             </Router>
