61aa650027fd6e74c8def04b665456ff8b7df527
Kreesan Govender <kreesangovender12@gmail.com>
2024-08-09 01:00:22 +0200

    m1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Carousel from './components/Carousel';
 import Menu from './components/Menu';
-import Basket from './components/Basket';
 import FrontPageBasket from './components/FrontPageBasket';
 import Checkout from './components/Checkout';
 import Orders from './components/Orders';
 import AboutUs from './components/AboutUs';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
-import { BasketProvider } from './contexts/BasketContext';
+import { BasketProvider, BasketContext } from './contexts/BasketContext';
 import './App.css';
 
 function App() {
+    const { basket } = useContext(BasketContext);
 
-        useEffect(() => {
-            fetch('https://fathomless-retreat-07632-66acd80d626f.herokuapp.com')
-                .then(response => response.json())
-                .then(data => console.log(data))
-                .catch(error => console.error('Error:', error));
-        }, []);
-
+    useEffect(() => {
+        fetch('https://fathomless-retreat-07632-66acd80d626f.herokuapp.com')
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    }, []);
 
     return (
         <BasketProvider>
@@ -32,13 +31,13 @@ function App() {
                         <Route path="/" exact>
                             <Carousel />
                             <Menu />
+                            {basket.length > 0 && <FrontPageBasket />} {/* Show only if items in basket */}
                         </Route>
                         <Route path="/checkout" component={Checkout} />
                         <Route path="/orders" component={Orders} />
                         <Route path="/about-us" component={AboutUs} />
                         <Route path="/contact-us" component={ContactUs} />
                     </Switch>
-                    <FrontPageBasket /> 
                     <Footer />
                 </div>
             </Router>
