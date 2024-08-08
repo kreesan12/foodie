995e2c6a9173cc55fd3aeb17b9dfab128f6835bb
Kreesan Govender <kreesangovender12@gmail.com>
2024-08-08 18:55:01 +0200

    08aug2024_002

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import './Footer.css';
 function Footer() {
     return (
         <footer className="footer">
+            <p>&copy; 2024 Divineveg on 262. All Rights Reserved.</p>
             <div className="footer-social-links">
                 <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
                 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
